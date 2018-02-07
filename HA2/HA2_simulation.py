584aee8db99f1fffd2d91fb3bc84095807f1b831
David <david.kasto@gmail.com>
2018-02-07 22:19:38 +0100

    hejhej

--- HA2/HA2_simulation.py
+++ HA2/HA2_simulation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #coding=utf-8
 
-# # imports # #
+# # # imports # # #
 import numpy as np
 from gpaw import GPAW, Mixer, PW
 from ase.build import *
@@ -14,57 +14,57 @@ from ase.visualize import view
 from ase.parallel import rank
 from ase import Atoms
 
-#from ase.lattice.surface import *
+# # # Constants and parameters # # #
+a_al = 4.05 # Lattice constant for Al, experimentally determined
+N_lattice_spacings = 7 # Number of lattice constants to loop over to find equilibrium
+E_al = 84.67567  # Ionization energy for hardest bound core electron
 
-a_al = 4.05
-al = bulk('Al', 'fcc', a=a_al, cubic=False)
+# # # Create Al bulk and initialize calculator parameters # # #
+al = bulk('Al', 'fcc', a=a_al, cubic=False) # Create Al bulk
+mixer = Mixer(beta=0.1,	nmaxold=5,	weight=50.0) # Recommended values for small systems
+E_cut = [50, E_al, 100, 200, 300, 400, 500]  # cut-off energy
 
-mixer = Mixer(beta=0.1,	nmaxold=5,	weight=50.0)
-N_lattice_spacings = 7
-E_al = 84.67567  # ionization energy for hardest bound core electron
-E_cut = [50, E_al, 100, 200, 300, 400, 500, 600, 700, 800]  # cut-off energy
-
-for energy in [500]:
+for energy in [500]: # Change to E_cut to loop and check convergence
     calc = GPAW(mode=PW(energy),  # use the LCAO basis mode
-                h=0.18,  # grid spacing
-                xc='PBE',  # XC-functional
+                h=0.18,  # grid spacing, recommended value in this course
+                xc='PBE',  # Exchange-correlation functional
                 mixer=mixer,
-                kpts=(12, 12, 12),  # k-point grid
+                kpts=(12, 12, 12),  # k-point grid - LOOP OVER LATER TO CHECK "CONVERGENCE"
                 txt='out.txt')  # name of GPAW output text file
-
     al.set_calculator(calc)
-    cell_0 = al.cell
-    for eps in np.linspace(-0.02, 0.02,	N_lattice_spacings):
-        al.cell = (1 + eps) * cell_0
-        al.get_potential_energy() #ghj
 
-    confs = read('out.txt@0:7')  # read 7 conﬁgurations
+    # # # Find lattice constant with lowest energy # # #
+    cell_0 = al.cell # Unit cell object of the Al bulk
+    for eps in np.linspace(-0.02, 0.02,	N_lattice_spacings):
+        al.cell = (1 + eps) * cell_0 # Adjust lattice constant of unit cell
+        al.get_potential_energy() # Calculate the potential energy for the Al bulk
+
+    confs = read('out.txt@0:'+str(N_lattice_spacings))  # Read the conﬁgurations
 
     # Extract volumes and energies:
     volumes = [atoms.get_volume() for atoms in confs]
     energies = [atoms.get_potential_energy() for atoms in confs]
     # if rank == 0:
     #     print energies, shape(energies)
+
+    # Plot energies as a function of unit cell volume (directly related to latt. const.)
     eos = EquationOfState(volumes, energies)
     v0, E_bulk, B = eos.fit()
     eos.plot('Al_eos.png')
-    a_calc = (4 * v0)**(1 / 3.0) # Why is this correct?
+    a_calc = (4 * v0)**(1 / 3.0) # Latt. const. acc. to ASE doc., but why is this correct?
 
-<<<<<<< HEAD
-    N_x = 1
-    N_y = 1
-    N_z = 3
-
-=======
+    # # # Create Al FCC structures (111 and 100) with 2x2 atoms on the
+    #     surface and 10 layers # # #
     N_x = 2
     N_y = 2
     N_z = 10
->>>>>>> 12248824f33037b0e270797f367ffa37d11ede25
+
     surface111 = fcc111('Al', size=(N_x, N_y, N_z), a=a_calc, vacuum=7.5)
     surface100 = fcc100('Al', size=(N_x, N_y, N_z), a=a_calc, vacuum=7.5)
     surface111.center(axis=2)
     surface100.center(axis=2)
 
+    # Initialize new calculator 
     calc2 = GPAW(mode=PW(energy),  # use the LCAO basis mode
                 h=0.18,  # grid spacing
                 xc='PBE',  # XC-functional
