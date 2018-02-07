f6b9fc5aa4be197e2c411b290dfe80c6814376f5
David <david.kasto@gmail.com>
2018-02-07 11:27:24 +0100

    SomeActualChanges

--- HA2/HA2_data_processing.py
+++ HA2/HA2_data_processing.py
@@ -1,49 +1,20 @@
 
-from ase.io import read
-from ase.units import kJ
-from ase.utils.eos import EquationOfState
+#from ase.io import read
 from ase.build import fcc111, fcc100
 from ase.cluster.wulff import wulff_construction
 from ase.visualize import view
-<<<<<<< HEAD
 import matplotlib.pyplot as plt
-=======
+import numpy as np
 
-# conﬁgs = read('out.txt@:' + str(N_lattice_spacings))  # read 7 conﬁgurations
->>>>>>> 4455cd8a5ce41bb446d8c426a901089040156ea6
-conﬁgs = read('HA2/out.txt@0:7')  # read 7 conﬁgurations
+# input file
+filename1 = 'sigmas.txt'
 
-# Extract volumes and energies:
-volumes = [atoms.get_volume() for atoms in conﬁgs]
-energies = [atoms.get_potential_energy() for atoms in conﬁgs]
-eos = EquationOfState(volumes, energies)
-print(volumes)
-print(energies)
-plt.plot(volumes)
-plt.show()
-v0, E_bulk, B = eos.ﬁt()
-eos.plot('Al_eos.png')
+# import and manage data
+sigmas = np.array(np.loadtxt(filename1))
+sigma111 = sigmas[0]
+sigma100 = sigmas[1]
 
-a_calc = (4 * v0)**(1 / 3.0)  # Is this correct?
-
-N_x = 2
-N_y = 2
-N_z = 3
-
-surface111 = fcc111('Al', a=a_calc, size=(N_x, N_y, N_z))
-surface100 = fcc100('Al', a=a_calc, size=(N_x, N_y, N_z))
-
-surface111.set_calculator(asdasdasd)
-surface100.set_calculator(asdasdasd)
-
-cell111 = surface111.get_cell()
-area111 = np.linalg.norm(np.cross(cell[0], cell[1]))
-sigma111 = (1 / (2 * area111)) * (surface111.get_potential_energy() - N_x * N_y * E_bulk)
-
-cell100 = surface111.get_cell()
-area100 = np.linalg.norm(np.cross(cell[0], cell[1]))
-sigma100 = (1 / (2 * area100)) * (surface100.get_potential_energy() - N_x * N_y * E_bulk)
-
+print(sigma111, sigma100)
 
 atoms = wulff_construction('Al',
                            surfaces=[(1, 0, 0),
--- HA2/HA2_simulation.py
+++ HA2/HA2_simulation.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
+#coding=utf-8
 
 # # imports # #
 import numpy as np
 from gpaw import GPAW, Mixer, PW
-from ase.build import bulk
+from ase.build import *
 from ase.io import read
-from ase.units import kJ
-from ase.utils.eos import EquationOfState
+from ase.units import kJ, J, m
+from ase.eos import EquationOfState
 from ase.build import fcc111, fcc100, add_adsorbate
 from ase.cluster.wulff import wulff_construction
 from ase.visualize import view
+from ase.parallel import rank
+#from ase.lattice.surface import *
 
 a_al = 4.05
 al = bulk('Al', 'fcc', a=a_al, cubic=False)
@@ -27,76 +30,57 @@ for energy in [500]:
                 kpts=(12, 12, 12),  # k-point grid
                 txt='out.txt')  # name of GPAW output text file
 
-al.set_calculator(calc)
-cell_0 = al.cell
-for eps in np.linspace(-0.02,	0.02,	N_lattice_spacings):
-    al.cell = (1 + eps) * cell_0
-    al.get_potential_energy()
-<<<<<<< HEAD
+    al.set_calculator(calc)
+    cell_0 = al.cell
+    for eps in np.linspace(-0.02, 0.02,	N_lattice_spacings):
+        al.cell = (1 + eps) * cell_0
+        al.get_potential_energy() #ghj
 
+    confs = read('out.txt@0:7')  # read 7 conﬁgurations
 
-conﬁgs = read('out.txt@:' + str(N_lattice_spacings))  # read 7 conﬁgurations
+    # Extract volumes and energies:
+    volumes = [atoms.get_volume() for atoms in confs]
+    energies = [atoms.get_potential_energy() for atoms in confs]
+    # if rank == 0:
+    #     print energies, shape(energies)
+    eos = EquationOfState(volumes, energies)
+    v0, E_bulk, B = eos.fit()
+    eos.plot('Al_eos.png')
+    a_calc = (4 * v0)**(1 / 3.0) # Is this correct?
 
-# Extract volumes and energies:
-volumes = [atoms.get_volume() for atoms in conﬁgs]
-energies = [atoms.get_potential_energy() for atoms in conﬁgs]
-eos = EquationOfState(volumes, energies)
-v0, E_bulk, B = eos.ﬁt()
-eos.plot('Al_eos.png')
-a_calc = (4 * v0)**(1 / 3.0)  # Is this correct?
+    N_x = 1
+    N_y = 1
+    N_z = 3
 
-N_x = 2
-N_y = 2
-N_z = 3
+    surface111 = fcc111('Al', size=(N_x, N_y, N_z), a=a_calc, vacuum=7.5)
+    surface100 = fcc100('Al', size=(N_x, N_y, N_z), a=a_calc, vacuum=7.5)
+    surface111.center(axis=2)
+    surface100.center(axis=2)
 
-surface111 = fcc111('Al', a=a_calc, size=(N_x, N_y, N_z))
-surface100 = fcc100('Al', a=a_calc, size=(N_x, N_y, N_z))
+    calc2 = GPAW(mode=PW(energy),  # use the LCAO basis mode
+                h=0.18,  # grid spacing
+                xc='PBE',  # XC-functional
+                mixer=mixer,
+                kpts=(12, 12, 1),  # k-point grid
+                txt='out2.txt')  # name of GPAW output text file
 
-surface111.set_calculator(calc)
-surface100.set_calculator(calc)
+    surface111.set_calculator(calc2)
+    surface100.set_calculator(calc2)
 
-cell111 = surface111.get_cell()
-area111 = np.linalg.norm(np.cross(cell[0], cell[1]))
-sigma111 = (1 / (2 * area111)) * (surface111.get_potential_energy() - N_x * N_y * E_bulk)
+    cell111 = surface111.get_cell()
+    area111 = np.linalg.norm(np.cross(cell111[0], cell111[1]))
+    surfEn111 = surface111.get_potential_energy()
+    cell100 = surface100.get_cell()
+    area100 = np.linalg.norm(np.cross(cell100[0], cell100[1]))
+    surfEn100 = surface100.get_potential_energy()
 
-cell100 = surface111.get_cell()
-area100 = np.linalg.norm(np.cross(cell[0], cell[1]))
-sigma100 = (1 / (2 * area100)) * (surface100.get_potential_energy() - N_x * N_y * E_bulk)
+    sigma111 = (1 / (2.0 * area111)) * (surfEn111 - N_x * N_y * E_bulk)
+    sigma100 = (1 / (2.0 * area100)) * (surfEn100 - N_x * N_y * E_bulk)
 
+    file = open('sigmas.txt','w')
+    file.write(str(sigma111)+'\t'+str(sigma100))
+    file.close()
 
-al_construction = wulff_construction('Al',
-                                     surfaces=[(1, 0, 0),
-                                               (1, 1, 1)],
-                                     energies=[sigma100, sigma111],
-                                     size=10000,
-                                     structure='fcc',
-                                     rounding='below')  # Vad gör denna?
-al_construction.center(vacuum=10)
-
-view(al_construction)
-
-
-add_adsorbate(slab=surface100, adsorbate='CO', height=4.5, position='ontop')
-add_adsorbate(slab=surface111, adsorbate='CO', height=4.5, position='ontop')
-
-cell111 = surface111.get_cell()
-area111 = np.linalg.norm(np.cross(cell[0], cell[1]))
-sigma111 = (1 / (2 * area111)) * (surface111.get_potential_energy() - N_x * N_y * E_bulk)
-
-cell100 = surface111.get_cell()
-area100 = np.linalg.norm(np.cross(cell[0], cell[1]))
-sigma100 = (1 / (2 * area100)) * (surface100.get_potential_energy() - N_x * N_y * E_bulk)
-
-al_construction_adsorbate = wulff_construction('Al',
-                                               surfaces=[(1, 0, 0),
-                                                         (1, 1, 1)],
-                                               energies=[sigma100, sigma111],
-                                               size=10000,
-                                               structure='fcc',
-                                               rounding='below')  # Vad gör denna?
-al_construction_adsorbate(vacuum=10)
-
-view(al_construction_adsorbate)
-=======
-    print al.get_potential_energy()
->>>>>>> 4455cd8a5ce41bb446d8c426a901089040156ea6
+    if rank == 0:
+        a = 0
+        print area111, surfEn111, E_bulk, sigma111
