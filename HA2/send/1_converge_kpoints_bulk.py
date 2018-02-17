fe42421af4520576595f68b517647cc6558ed89c
aanner <albin.anner@gmail.com>
2018-02-17 10:52:03 +0100

    asdasd

--- HA2/send/1_converge_kpoints_bulk.py
+++ HA2/send/1_converge_kpoints_bulk.py
@@ -14,6 +14,9 @@ from ase.visualize import view
 from ase.parallel import rank
 from ase import Atoms
 import sys
+import os
+
+homedir = os.path.expanduser('~')
 
 experimental_lattice_parameter = 4.05
 
@@ -22,10 +25,10 @@ mixer = Mixer(beta=0.1,	nmaxold=5,	weight=50.0)  # Recommended values for small 
 al_bulk = bulk('Al', 'fcc', a=experimental_lattice_parameter, cubic=False)
 
 # # # Vary number of k-points to find a convergent number # # #
-n_k_points = [4, 5, 6, 7, 8, 9, 10]
+n_k_points = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
 # # # Cutoff energy chosen somewhat arbitrarily, convergence checked in next program # # #
-energy_cutoff = 300
+energy_cutoff = 200
 energies = []
 for n in n_k_points:
     if rank == 0:
@@ -36,14 +39,14 @@ for n in n_k_points:
                 xc='PBE',  # Exchange-correlation functional
                 mixer=mixer,  # Mixer
                 # k-point grid - LOOP OVER LATER TO CHECK "CONVERGENCE"
-                kpts=(n_k_points, n_k_points, n_k_points),
+                kpts=(n, n, n),
                 txt='simulate_bulk_Al_GPAW.txt')  # name of GPAW output text file
 
     al_bulk.set_calculator(calc)
     total_energy = al_bulk.get_potential_energy()
     energies.append(total_energy)
 
-with open('~/TIF035/HA2/bulk/1_converge_kpoints_bulk.txt', 'w') as textfile:
+with open(homedir + '/TIF035/HA2/bulk/1_converge_kpoints_bulk.txt', 'w') as textfile:
     textfile.write('number of k points, bulk_energy\n')
     for i in range(len(n_k_points)):
-        textfile.write(str(n_k_points[i]) + ',' + str(energies[i]))
+        textfile.write(str(n_k_points[i]) + ',' + str(energies[i]) + '\n')
