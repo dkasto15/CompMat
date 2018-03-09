9f6c9ca91f9c4a42bf575e6aa8a0a864d9cf9361
dkasto15 <david.kasto@gmail.com>
2018-03-09 11:23:14 +0100

    dg

--- HA4/code/elastic_properties.py
+++ HA4/code/elastic_properties.py
@@ -7,6 +7,7 @@ from ase.eos import EquationOfState
 from ase.atoms import copy
 from ase.build import bulk
 from ase.phonons import Phonons
+from ase.dft.kpoints import ibz_points, bandpath
 global al_bulk
 
 
@@ -53,12 +54,12 @@ def main():
     e2 = e1
     ep_mat_12 = np.array([[e1, 0, 0], [0, e2, 0], [0, 0, 0]])
     energies.append(al_bulk.get_potential_energy())
+    V = al_bulk.get_volume() # Equilibrium cell volume
     print(al_bulk.get_volume())
     cell_0 = al_bulk.get_cell()
     al_bulk.set_cell(np.dot((np.eye(3) + ep_mat_12), cell_0))
     print(al_bulk.get_volume())
     energies.append(al_bulk.get_potential_energy())
-    V = al_bulk.get_volume()
     delta_E = energies[-1] - energies[0]
     C11_plus_C12 = delta_E / (V * e1**2)
     print(C11_plus_C12)
@@ -82,7 +83,59 @@ def main():
     B = (C11 + 2 * C12) / 3
     c_prim = (C11 * C12) / 2
 
-    
+    ''' Phonon calculator '''
+    al_bulk = bulk('Al', 'fcc', a=4.032)
+    N = 7
+    ph = Phonons(al_bulk, calc, supercell=(N, N, N), delta=0.05)
+    ph.run()
+
+    # Read forces and assemble the dynamical matrix
+    ph.read(acoustic=True)
+
+    # High-symmetry points in the Brillouin zone
+    points = ibz_points['fcc']
+    G = points['Gamma']
+    X = points['X']
+    W = points['W']
+    K = points['K']
+    L = points['L']
+    U = points['U']
+
+    point_names = ['$\Gamma$', 'X', 'U', 'L', '$\Gamma$', 'K']
+    path = [G, X, U, L, G, K]
+
+    # Band structure in meV
+    path_kc, q, Q = bandpath(path, al_bulk.cell, 100)
+    omega_kn = 1000 * ph.band_structure(path_kc)
+
+    # Calculate phonon DOS
+    omega_e, dos_e = ph.dos(kpts=(50, 50, 50), npts=5000, delta=5e-4)
+    omega_e *= 1000
+
+    # Plot the band structure and DOS
+    import matplotlib.pyplot as plt
+    plt.figure(1, (8, 6))
+    plt.axes([.1, .07, .67, .85])
+    for n in range(len(omega_kn[0])):
+        omega_n = omega_kn[:, n]
+        plt.plot(q, omega_n, 'k-', lw=2)
+
+    plt.xticks(Q, point_names, fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.xlim(q[0], q[-1])
+    plt.ylabel("Frequency ($\mathrm{meV}$)", fontsize=22)
+    plt.grid('on')
+
+    plt.axes([.8, .07, .17, .85])
+    plt.fill_between(dos_e, omega_e, y2=0, color='lightgrey', edgecolor='k', lw=1)
+    plt.ylim(0, 35)
+    plt.xticks([], [])
+    plt.yticks([], [])
+    plt.xlabel("DOS", fontsize=18)
+    plt.show()
+
+    print(omega_e)
+    print(omega_n)
 
 #     ''' Observed output data '''
 #     a0 = 4.032
